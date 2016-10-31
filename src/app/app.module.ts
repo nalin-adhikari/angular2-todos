eda5a81889568c109c2cefee7764d427ecb8f85d
Nalin Adhikari <csitplay@gmail.com>
2016-11-01 01:12:59 +0545

    angular2 todos project created

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AddComponent } from './components/add/add.component';
+import { DisplayComponent } from './components/display/display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddComponent,
+    DisplayComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
